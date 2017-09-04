4c33e8d2b24a71fa4760ade5b5f4709cbe54ab2d
sanjeev77 <sanjeev@happay.in>
2017-09-04 21:58:23 +0530

    final changes are done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import fullCalendar from "fullcalendar";
 import moment from 'moment';
 import FullcalendarObjectMapper from './fullcalendarObjectMapper';
-// import '../node_modules/fullcalendar/dist/fullcalendar.min.css'
+
 export default class FullCalendar extends React.Component{
 	constructor(){
 		super();
@@ -19,18 +19,13 @@ export default class FullCalendar extends React.Component{
 	}
 
   	componentWillReceiveProps(nextProps){
-  		// console.log(nextProps);
   		this.jq(`#${this.root}`).fullCalendar('destroy');
   		const objectMapperSettings = this.fullcalendarObjectMapper.getSettings(nextProps);
     	this.instance = this.jq(`#${this.root}`).fullCalendar(objectMapperSettings);
-    	// $(`#${this.root}`).fullCalendar( 'render' )
-    	/*this.jq(`#${this.root}`).fullCalendar('removeEvents');
-        this.jq(`#${this.root}`).fullCalendar( 'addEventSource', nextProps.events);         
-        this.jq(`#${this.root}`).fullCalendar( 'refetchEvents' );*/
   	}
 
 	render(){
-		this.root = this.props.root || 'ID' + new Date.getTime(); 
+		this.root = this.props.id || 'ID' + new Date.getTime(); 
 		return(
 			<div id={this.root}></div>
 		)
